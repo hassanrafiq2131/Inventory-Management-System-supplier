0d2d45bc8c8236aff81aeb4b91889084785079b2
Ashar Amir <ashharamir@gmail.com>
2024-12-07 23:28:45 +0500

    changes

--- server/config/firebase-admin.js
+++ server/config/firebase-admin.js
@@ -5,28 +5,15 @@ import { dirname, join } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// try {
-//     admin.initializeApp({
-//         // Use the service account file
-//         credential: admin.credential.cert(join(__dirname, '../../inventory-mgmt-sys-d2e11-firebase-adminsdk-4yuvh-4fb4199357.json')),
-//     });
-//     console.log('Firebase Admin initialized successfully');
-// } catch (error) {
-//     console.error('Firebase Admin initialization error:', error);
-// }
-
 try {
-    // Parse the service account key from the environment variable
-    const serviceAccountKey = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
-
-    // Initialize Firebase Admin SDK
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccountKey),
+        // Use the service account file
+        credential: admin.credential.cert(join(__dirname, '../../inventory-mgmt-sys-d2e11-firebase-adminsdk-4yuvh-4fb4199357.json')),
     });
-
     console.log('Firebase Admin initialized successfully');
 } catch (error) {
     console.error('Firebase Admin initialization error:', error);
 }
+
 
 export default admin;
